847793f36355a6eeef0a7b24ac35533fface47f1
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-08 12:40:26 -0300

    Finished password recover and reset feature

--- 09-continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ 09-continuando-back-end-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,7 @@
 // import User from '@modules/users/infra/typeorm/entities/Users';
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
+
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -29,12 +31,29 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido',
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
     );
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password/?token=${token}`,
+        },
+      },
+    });
   }
 }
 
--- 09-continuando-back-end-do-app/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ 09-continuando-back-end-do-app/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,4 +1,5 @@
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 interface IMessage {
   to: string;
@@ -6,12 +7,9 @@ interface IMessage {
 }
 
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[] = [];
+  private messages: ISendMailDTO[] = [];
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({
-      to,
-      body,
-    });
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.messages.push(message);
   }
 }
