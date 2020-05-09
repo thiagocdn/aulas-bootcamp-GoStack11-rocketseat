88db18d684d2f152f07e46dd32f094eab9635252
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-09 14:35:34 -0300

    Added appointment filter for providers by month

--- 09-continuando-back-end-do-app/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ 09-continuando-back-end-do-app/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,7 +1,9 @@
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -14,6 +16,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
     );
 
     return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+
+    return appointments;
   }
 
   public async create({
---
+++ 09-continuando-back-end-do-app/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,40 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequestDTO {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponseDTO = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+export default class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequestDTO): Promise<IResponseDTO> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    console.log(appointments);
+
+    return [{ day: 1, available: false }];
+  }
+}
