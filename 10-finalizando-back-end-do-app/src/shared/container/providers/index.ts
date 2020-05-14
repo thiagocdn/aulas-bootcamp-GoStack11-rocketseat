d311e0de56373a5ca495c27e3154957b4e8bb5cd
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-14 11:00:06 -0300

    Added SES Mail Provider

--- 10-finalizando-back-end-do-app/src/shared/container/providers/index.ts
+++ 10-finalizando-back-end-do-app/src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from 'config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
