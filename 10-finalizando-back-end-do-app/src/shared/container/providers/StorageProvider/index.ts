c79fb0607033c5b82b821533b308a7b02ba830ff
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-14 12:45:15 -0300

    Injection adjustments

---
+++ 10-finalizando-back-end-do-app/src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers.disk,
+);
--- 10-finalizando-back-end-do-app/src/shared/container/providers/index.ts
+++ 10-finalizando-back-end-do-app/src/shared/container/providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from 'config/mail';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
