c035c51651009fc3cb2dbc3cad53ef2d089dcc4b
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-15 00:26:54 -0300

    Added requests limit with rate-limiter-flexible

--- 10-finalizando-back-end-do-app/src/shared/infra/http/server.ts
+++ 10-finalizando-back-end-do-app/src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/RateLimiter';
 
 import routes from './routes';
 
@@ -15,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
