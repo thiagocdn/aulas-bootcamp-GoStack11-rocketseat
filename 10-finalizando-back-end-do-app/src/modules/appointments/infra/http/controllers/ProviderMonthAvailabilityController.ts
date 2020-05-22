e17dc54f665bbb7c930c484b6ec5203de1999c1d
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-22 11:44:36 -0300

    Changed body params to query params in GET routes

--- 10-finalizando-back-end-do-app/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ 10-finalizando-back-end-do-app/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailabilityService,
@@ -14,8 +14,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
