f08c61b6e788e9f4624d8d485ff404330d57cc3a
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-22 12:20:53 -0300

    cache saving adjustments

--- 10-finalizando-back-end-do-app/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ 10-finalizando-back-end-do-app/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -19,6 +20,6 @@ export default class ProviderAppointmentsController {
       year: Number(year),
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
--- 10-finalizando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ 10-finalizando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -43,7 +44,7 @@ export default class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     //
