ce052cb3f262a89a37d533785380507e36b29617
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-05-22 11:53:48 -0300

    fixed appointment availability checking

--- 10-finalizando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
+++ 10-finalizando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
@@ -52,6 +52,7 @@ class CreateAppointmentService {
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
