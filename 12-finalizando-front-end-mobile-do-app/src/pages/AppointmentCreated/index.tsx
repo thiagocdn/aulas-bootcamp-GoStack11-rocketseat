68992f075dd491d318545317ab55f6a4fb7eebcb
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-11 17:14:35 -0300

    Adicionado tela de confirmação de agendamento

--- 12-finalizando-front-end-mobile-do-app/src/pages/AppointmentCreated/index.tsx
+++ 12-finalizando-front-end-mobile-do-app/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04D361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Qualquer coisa para ter alguma coisa</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
