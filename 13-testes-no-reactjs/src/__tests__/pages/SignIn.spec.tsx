c7dbb3b571f44a55fa09a07105f96481f451a3f9
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-17 19:26:31 -0300

    Teste do componente SignIn sem mock

--- 13-testes-no-reactjs/src/__tests__/pages/SignIn.spec.tsx
+++ 13-testes-no-reactjs/src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard')
   });
 });
