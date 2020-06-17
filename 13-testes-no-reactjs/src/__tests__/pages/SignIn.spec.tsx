30ffaafa715a95aacc9b1a918e8942af727b7adb
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-17 18:11:25 -0300

    Iniciado primeiro teste, renderização tela SignIn

---
+++ 13-testes-no-reactjs/src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', () => {
+    const { debug } = render(<SignIn />);
+
+    debug();
+  });
+});
